cdc18dd1de45a6212fb76e502d5677f3cf595037
ymandal <127987142+ymandal@users.noreply.github.com>
2024-12-23 21:25:12 -0500

    Create Source1_printtest.R
    Initial testing for empty skeleton

---
+++ Subsets/Source1_printtest.R
@@ -0,0 +1,10 @@
+
+
+helloworld <- "Hello World"
+
+print(helloworld)
+
+
+#Transitfeeds3 <- readr::read_csv("https://raw.githubusercontent.com/ymandal/NavApp_PublicAccess/refs/heads/main/MobilityData/cleaner-MobilityData2.csv")
+
+#print(Transitfeeds3)
