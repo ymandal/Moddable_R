e675f13c3562f56eb559309d91286440ec1335bb
ymandal <127987142+ymandal@users.noreply.github.com>
2024-12-23 22:06:18 -0500

    Update sole_script.R

--- Main/sole_script.R
+++ Main/sole_script.R
@@ -1,5 +1,10 @@
 
-#initial contains libraries of:
-library( c(
-  "tidyverse","devtools")
-#-----
+#shiny leaflet demo
+
+s1 <- "https://raw.githubusercontent.com/ymandal/Moddable_R/refs/heads/main/Subsets/Source1_printtest.R" # rend_aj.R
+#s2<-"https://github.com/juanchiem/R-sources/blob/master/theme_juan.R?raw=TRUE" # theme_juan.R
+#s3<-"https://github.com/juanchiem/R-sources-Juan/blob/master/assumptions.R?raw=TRUE" # lm_assumptions.R
+
+devtools::source_url(s1)
+#devtools::source_url(s2)
+#devtools::source_url(s3)
