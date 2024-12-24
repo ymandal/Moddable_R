f4e0ad30824df5955bab4790a0869f99fc6ac39c
ymandal <127987142+ymandal@users.noreply.github.com>
2024-12-23 22:46:10 -0500

    Update remoteUI_internal.R

--- Main/remoteUI_internal.R
+++ Main/remoteUI_internal.R
@@ -1,7 +1,7 @@
 
 
-bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}, .selectize-input { padding: 2px; min-height: 0;} #country_selection + div>.selectize-dropdown{width: 30px !important;}") #note w/o this line, the code functions but content below doesn't start appearing then, just a grey screen
+#bootstrapPage(
+    #tags$style(type = "text/css", "html, body {width:100%;height:100%}, .selectize-input { padding: 2px; min-height: 0;} #country_selection + div>.selectize-dropdown{width: 30px !important;}"), #note w/o this line, the code functions but content below doesn't start appearing then, just a grey screen
     
     
     ##the Main Map: 
