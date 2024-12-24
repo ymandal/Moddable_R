01b1998ae86329a0c30028be787e1bec8cb815c8
ymandal <127987142+ymandal@users.noreply.github.com>
2024-12-23 22:39:17 -0500

    Update remoteUI_internal.R

--- Main/remoteUI_internal.R
+++ Main/remoteUI_internal.R
@@ -1,1 +1,3 @@
- leafletOutput("themainmap", width = "100%", height = "100%")
+bootstrapPage(
+    tags$style(type = "text/css", "html, body {width:100%;height:100%}, .selectize-input { padding: 2px; min-height: 0;} #country_selection + div>.selectize-dropdown{width: 30px !important;}") #note w/o this line, the code functions but content below doesn't start appearing then, just a grey screen
+  )
